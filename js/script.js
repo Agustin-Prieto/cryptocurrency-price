297bf93f5f92a88c7dbe767dbacd236171ef3cd0
Agustin Prieto <agustinprieto.ar@gmail.com>
2017-09-19 19:05:12 -0300

    styles added

--- js/script.js
+++ js/script.js
@@ -1,52 +1,3 @@
-// console.log(document.getElementById('priceContainer').content);
-
-function loadCurrencies(){
-	var xhr = new XMLHttpRequest();
-	var limit = '?limit=100';
-  
-	xhr.open('GET', 'https://api.coinmarketcap.com/v1/ticker/' + limit, true);
-
-	// optional
-	// xhr.onprogress = function(){
-	// 	console.log('READYSTATE: ', xhr.readyState);
-	// }
-
-	xhr.onerror = function(){
-		console.log('Request Error...');
-	}
-
-
-	xhr.onload = function(){
-		// console.log('READYSTATE: ', xhr.readyState);
-		if (this.status == 200) {
-			var currencies = JSON.parse(this.responseText);
-			var output = '';
-
-			for(var i in currencies){
-				output += 
-				`<tr>
-				 	<th scope="row">${currencies[i].rank}</th>
-					<td>${currencies[i].name}</td>
-					<td>$${currencies[i].price_usd}</td>
-					<td>$${currencies[i].market_cap_usd}</td>
-					<td>${currencies[i].total_supply} ${currencies[i].symbol}</td>
-					<td>${currencies[i].percent_change_24h}</td>
-		 		</tr>`;
-			}
-
-			document.getElementById('info').insertAdjacentHTML('beforeend', output);
-
-			// console.log(this.responseText);
-			
-		} else if (this.status == 404) {
-			console.log('Not Found');
-		}
-	}
-
-	// sends request
-	xhr.send();
-}
-
 function globalData(){
 	var xhr = new XMLHttpRequest();
   
@@ -63,13 +14,13 @@ function globalData(){
 			var output = '';
 
 				output += 
-				`<ul>
+				`<ul class="global_data-list">
 					<li>Market Cap: $${data.total_market_cap_usd}</li>
-					<li>total 24h volume usd: $${data.total_24h_volume_usd}</li>
-					<li>bitcoin percentage of market cap: ${data.bitcoin_percentage_of_market_cap}%</li>
-					<li>active currencies: ${data.active_currencies}</li>
-					<li>active assets: ${data.active_assets}</li>
-					<li>active markets: ${data.active_markets}</li>
+					<li>Total 24h Volume USD: $${data.total_24h_volume_usd}</li>
+					<li>Bitcoin Percentage Of Market Cap: ${data.bitcoin_percentage_of_market_cap}%</li>
+					<li>Currencies: ${data.active_currencies}</li>
+					<li>Assets: ${data.active_assets}</li>
+					<li>Markets: ${data.active_markets}</li>
 				</ul>`;
 
 			document.getElementById('globalData').innerHTML = output;
@@ -79,7 +30,44 @@ function globalData(){
 		}
 	}
 
-	// sends request
+	xhr.send();
+}
+
+function loadCurrencies(){
+	var xhr = new XMLHttpRequest();
+	var limit = '?limit=100';
+  
+	xhr.open('GET', 'https://api.coinmarketcap.com/v1/ticker/' + limit, true);
+
+	xhr.onerror = function(){
+		console.log('Request Error...');
+	}
+
+
+	xhr.onload = function(){
+		if (this.status == 200) {
+			var currencies = JSON.parse(this.responseText);
+			var output = '';
+
+			for(var i in currencies){			
+				output += 
+				`<tr>
+				 	<th scope="row">${currencies[i].rank}</th>
+					<td><a href="${currencies[i].name}">${currencies[i].name}</a></td>
+					<td>$${currencies[i].price_usd}</td>
+					<td>$${currencies[i].market_cap_usd}</td>
+					<td>${currencies[i].total_supply} ${currencies[i].symbol}</td>
+					<td class="coin-percent">${currencies[i].percent_change_24h}%</td>
+		 		</tr>`;
+			}
+
+			document.getElementById('info').insertAdjacentHTML('beforeend', output);
+
+		} else if (this.status == 404) {
+			console.log('Not Found');
+		}
+	}
+
 	xhr.send();
 }
 
