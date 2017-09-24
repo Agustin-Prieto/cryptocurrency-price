c010117c71896120e51b6385ea5f5b561fa39270
Agustin Prieto <agustinprieto.ar@gmail.com>
2017-09-24 20:06:01 -0300

    modularized code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+var imprData = function(data){
+	var output = '';
+			output += 
+			`<ul class="global_data-list">
+				<li>Market Cap: $${data.total_market_cap_usd}</li>
+				<li>Total 24h Volume USD: $${data.total_24h_volume_usd}</li>
+				<li>Bitcoin Percentage Of Market Cap: ${data.bitcoin_percentage_of_market_cap}%</li>
+				<li>Currencies: ${data.active_currencies}</li>
+				<li>Assets: ${data.active_assets}</li>
+				<li>Markets: ${data.active_markets}</li>
+			</ul>`;
+
+			document.getElementById('globalData').innerHTML = output;
+}
+
+var imprCurr = function(curr){
+	var output = '';
+
+		for(var i in curr){			
+			output += 
+			`<tr>
+			 	<th scope="row">${curr[i].rank}</th>
+				<td><a href="html/${curr[i].name}.html">${curr[i].name}</a></td>
+				<td>$${curr[i].price_usd}</td>
+				<td>$${curr[i].market_cap_usd}</td>
+				<td>${curr[i].total_supply} ${curr[i].symbol}</td>
+				<td class="coin-percent">${curr[i].percent_change_24h}%</td>
+		 	</tr>`;
+		}
+
+			document.getElementById('info').insertAdjacentHTML('beforeend', output);
+}
+
+var notFound = function(){
+	var output = '';
+	output += 
+	`
+		<h1>Site Not Found</h1>
+	`;
+}
+
 function globalData(){
 	var xhr = new XMLHttpRequest();
   
@@ -7,26 +48,13 @@ function globalData(){
 		console.log('Request Error...');
 	}
 
-
 	xhr.onload = function(){
 		if (this.status == 200) {
 			var data = JSON.parse(this.responseText);
-			var output = '';
-
-				output += 
-				`<ul class="global_data-list">
-					<li>Market Cap: $${data.total_market_cap_usd}</li>
-					<li>Total 24h Volume USD: $${data.total_24h_volume_usd}</li>
-					<li>Bitcoin Percentage Of Market Cap: ${data.bitcoin_percentage_of_market_cap}%</li>
-					<li>Currencies: ${data.active_currencies}</li>
-					<li>Assets: ${data.active_assets}</li>
-					<li>Markets: ${data.active_markets}</li>
-				</ul>`;
-
-			document.getElementById('globalData').innerHTML = output;
+			imprData(data);
 			
 		} else if (this.status == 404) {
-			console.log('Not Found');
+			notFound();
 		}
 	}
 
@@ -47,24 +75,10 @@ function loadCurrencies(){
 	xhr.onload = function(){
 		if (this.status == 200) {
 			var currencies = JSON.parse(this.responseText);
-			var output = '';
 
-			for(var i in currencies){			
-				output += 
-				`<tr>
-				 	<th scope="row">${currencies[i].rank}</th>
-					<td><a href="${currencies[i].name}">${currencies[i].name}</a></td>
-					<td>$${currencies[i].price_usd}</td>
-					<td>$${currencies[i].market_cap_usd}</td>
-					<td>${currencies[i].total_supply} ${currencies[i].symbol}</td>
-					<td class="coin-percent">${currencies[i].percent_change_24h}%</td>
-		 		</tr>`;
-			}
-
-			document.getElementById('info').insertAdjacentHTML('beforeend', output);
-
+			imprCurr(currencies);
 		} else if (this.status == 404) {
-			console.log('Not Found');
+			notFound();
 		}
 	}
 
